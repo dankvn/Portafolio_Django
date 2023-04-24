1e5a1638f76669312075a7dc7bbf27ace0a4d40f
unknown <cjmayk@hotmail.com>
2023-04-24 10:54:00 -0500

    init coomit

--- cv_dig/urls.py
+++ cv_dig/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import path , include 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 from cv import views
 
@@ -16,3 +18,8 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+
+urlpatterns = [
+    # tus otras urls aquí
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
